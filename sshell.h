291f560f283cce4a59b1111ca43ee0ed59cf5067
Scholarbee <99612272+Scholarbee@users.noreply.github.com>
2023-10-20 23:09:10 +0000

    s-shell

--- sshell.h
+++ sshell.h
@@ -43,11 +43,11 @@ typedef struct list_str
 {
 	int num;
 	char *p_str;
-	struct list_str *next;
+	struct list_str *nxt;
 } list_tbl;
 
 
-typedef struct argument_passed
+typedef struct my_argument_passed
 {
 	char *arg;
 	char **argv;
@@ -69,33 +69,33 @@ typedef struct argument_passed
 	int commandBufferType;
 	int readfd;
 	int history_number;
-} arg_info;
+} my_arg_info;
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
 
-typedef struct builtin
+typedef struct built_in
 {
 	char *type;
-	int (*func)(arg_info *);
-} builtin_table;
+	int (*func)(my_arg_info *);
+} built_in_tbl;
 
 
 /* prtotype main_shell */
-int ss_hsh(arg_info *, char **);
-int ss_find_builtin(arg_info *);
-void ss_find_cmd(arg_info *);
-void ss_fork_cmd(arg_info *);
+int ss_hsh(my_arg_info *, char **);
+int ss_find_builtin(my_arg_info *);
+void ss_find_cmd(my_arg_info *);
+void ss_fork_cmd(my_arg_info *);
 
 /* loophsh.c */
 int ss_loophsh(char **);
 
 /* prtotype for parse.c */
-int parse_is_cmd(arg_info *, char *);
+int parse_is_cmd(my_arg_info *, char *);
 char *parse_dup_chars(char *, int, int);
-char *parse_find_path(arg_info *, char *, char *);
+char *parse_find_path(my_arg_info *, char *, char *);
 
 /* prototype for handleErros1.c */
 void eh_eputs(char *);
@@ -132,55 +132,55 @@ void *aa_realloc(void *, unsigned int, unsigned int);
 int mem_bfree(void **);
 
 /* prototype for converter.c */
-int conv_interactive(arg_info *);
+int conv_interactive(my_arg_info *);
 int conv_is_delim(char, char *);
 int conv_isalpha(int);
 int conv_atoi(char *);
 
 /* prototype for handleErrors2.c */
 int eh_erratoi(char *);
-void eh_print_error(arg_info *, char *);
+void eh_print_error(my_arg_info *, char *);
 int eh_print_d(int, int);
 char *eh_convert_number(long int, int, int);
 void eh_remove_comments(char *);
 
 /* prototype for plugin1.c */
-int pi_myexit(arg_info *);
-int pi_mycd(arg_info *);
-int pi_myhelp(arg_info *);
+int pi_myexit(my_arg_info *);
+int pi_mycd(my_arg_info *);
+int pi_myhelp(my_arg_info *);
 
 /* prototype for plugin2.c */
-int pi_myhistory(arg_info *);
-int pi_myalias(arg_info *);
+int pi_myhistory(my_arg_info *);
+int pi_myalias(my_arg_info *);
 
 /* prototype for readInput.c */
-ssize_t gInput(arg_info *);
-int _gLine(arg_info *, char **, size_t *);
+ssize_t gInput(my_arg_info *);
+int _gLine(my_arg_info *, char **, size_t *);
 void sHandler(int);
 
 /* prototype for readInfo.c */
-void cInfo(arg_info *);
-void sInfo(arg_info *, char **);
-void fInfo(arg_info *, int);
+void cInfo(my_arg_info *);
+void sInfo(my_arg_info *, char **);
+void fInfo(my_arg_info *, int);
 
 /* prototype for env.c */
-char *env_my_get_env(arg_info *, const char *);
-int env_my_env(arg_info *);
-int env_my_set_env(arg_info *);
-int env_my_unset_env(arg_info *);
-int env_populate_env_list(arg_info *);
+char *env_my_get_env(my_arg_info *, const char *);
+int env_my_env(my_arg_info *);
+int env_my_set_env(my_arg_info *);
+int env_my_unset_env(my_arg_info *);
+int env_populate_env_list(my_arg_info *);
 
 /* prtotype for getEnv.c */
-char **_get_env(arg_info *);
-int _unset_env(arg_info *, char *);
-int _set_env(arg_info *, char *, char *);
+char **_get_env(my_arg_info *);
+int _unset_env(my_arg_info *, char *);
+int _set_env(my_arg_info *, char *, char *);
 
 /* prototype for past.c */
-char *get_past_file(arg_info *info);
-int write_past(arg_info *info);
-int read_past(arg_info *info);
-int build_past_list(arg_info *info, char *buf, int linecount);
-int renumber_past(arg_info *info);
+char *get_past_file(my_arg_info *info);
+int write_past(my_arg_info *info);
+int read_past(my_arg_info *info);
+int build_past_list(my_arg_info *info, char *buf, int linecount);
+int renumber_past(my_arg_info *info);
 
 /* prototype for array1.c */
 list_tbl *arr_add_node(list_tbl **, const char *, int);
@@ -197,10 +197,10 @@ list_tbl *arr_node_starts_with(list_tbl *, char *, char);
 ssize_t arr_get_node_index(list_tbl *, list_tbl *);
 
 /* prototype for var.c */
-int var_is_chain(arg_info *, char *, size_t *);
-void var_check_chain(arg_info *, char *, size_t *, size_t, size_t);
-int var_replace_alias(arg_info *);
-int var_replace_vars(arg_info *);
+int var_is_chain(my_arg_info *, char *, size_t *);
+void var_check_chain(my_arg_info *, char *, size_t *, size_t, size_t);
+int var_replace_alias(my_arg_info *);
+int var_replace_vars(my_arg_info *);
 int var_replace_string(char **, char *);
 
 #endif
