e73b8442b86a384efd4f61cf5f4d2d7f9befb8cb
Scholarbee <99612272+Scholarbee@users.noreply.github.com>
2023-10-21 02:01:05 +0000

    s-shell

--- readInput.c
+++ readInput.c
@@ -1,7 +1,7 @@
 #include "sshell.h"
 
 
-ssize_t input_buffer(my_arg_info *ptr_info, char **bfer, size_t *length)
+ssize_t input_buffer(my_arg_info *info, char **buf, size_t *length)
 {
 	ssize_t r = 0;
 	size_t len_p = 0;
@@ -10,13 +10,13 @@ ssize_t input_buffer(my_arg_info *ptr_info, char **bfer, size_t *length)
 	if (!*length)
 	{
 		
-		free(*bfer);
-		*bfer = NULL;
+		free(*buf);
+		*buf = NULL;
 		signal(SIGINT, sHandler);
 #if USE_GETLINE
-		r = _gLine(bfer, &len_p, stdin);
+		r = _gLine(buf, &len_p, stdin);
 #else
-		r = _gLine(ptr_info, bfer, &len_p);
+		r = _gLine(info, buf, &len_p);
 #endif
 		if (r > 0)
 		{
@@ -26,13 +26,13 @@ ssize_t input_buffer(my_arg_info *ptr_info, char **bfer, size_t *length)
 				(*buf)[r - 1] = '\0';
 				r--;
 			}
-			ptr_info->my_lc_flag = 1;
-			eh_remove_comments(*bfer);
+			info->my_lc_flag = 1;
+			eh_remove_comments(*buf);
 			build_past_list(info, *buf, info->my_history_number++);
 			
 			{
 				*length = r;
-				info->my_commandBuffer = bfer;
+				info->my_commandBuffer = buf;
 			}
 		}
 	}
@@ -40,26 +40,26 @@ ssize_t input_buffer(my_arg_info *ptr_info, char **bfer, size_t *length)
 }
 
 
-ssize_t gInput(my_arg_info *ptr_info)
+ssize_t gInput(my_arg_info *info)
 {
-	static char *bfer; 
+	static char *buf; 
 	static size_t i, j, len;
 	ssize_t r = 0;
-	char **buf_p = &(ptr_info->my_arg), *p;
+	char **buf_p = &(info->my_arg), *p;
 
 	ut_putchar(BUF_FLUSH);
-	r = input_buffer(ptr_info, &bfer, &len);
+	r = input_buffer(info, &buf, &len);
 	if (r == -1) 
 		return (-1);
 	if (len)
 	{
 		j = i;
-		p = bfer + i;
+		p = buf + i;
 
-		var_check_chain(ptr_info, bfer, &j, i, len);
+		var_check_chain(info, buf, &j, i, len);
 		while (j < len)
 		{
-			if (var_is_chain(ptr_info, bfer, &j))
+			if (var_is_chain(info, buf, &j))
 				break;
 			j++;
 		}
@@ -68,7 +68,7 @@ ssize_t gInput(my_arg_info *ptr_info)
 		if (i >= len)
 		{
 			i = len = 0;
-			ptr_info->my_commandBufferType = CMD_NORM;
+			info->my_commandBufferType = CMD_NORM;
 		}
 
 		*buf_p = p;
@@ -80,20 +80,20 @@ ssize_t gInput(my_arg_info *ptr_info)
 }
 
 
-ssize_t read_buffer(my_arg_info *ptr_info, char *buf, size_t *size)
+ssize_t read_buffer(my_arg_info *info, char *buf, size_t *size)
 {
 	ssize_t r = 0;
 
 	if (*size)
 		return (0);
-	r = read(ptr_info->my_readfd, buf, READ_BUF_SIZE);
+	r = read(info->my_readfd, buf, READ_BUF_SIZE);
 	if (r >= 0)
 		*size = r;
 	return (r);
 }
 
 
-int _gLine(my_arg_info *ptr_info, char **ptr, size_t *length)
+int _gLine(my_arg_info *info, char **ptr, size_t *length)
 {
 	static char buf[READ_BUF_SIZE];
 	static size_t i, len;
@@ -107,7 +107,7 @@ int _gLine(my_arg_info *ptr_info, char **ptr, size_t *length)
 	if (i == len)
 		i = len = 0;
 
-	r = read_buffer(ptr_info, buf, &len);
+	r = read_buffer(info, buf, &len);
 	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
 
@@ -118,9 +118,9 @@ int _gLine(my_arg_info *ptr_info, char **ptr, size_t *length)
 		return (p ? free(p), -1 : -1);
 
 	if (s)
-		qt_strncat(new_p, bfer + i, k - i);
+		qt_strncat(new_p, buf + i, k - i);
 	else
-		qt_strncpy(new_p, bfer + i, k - i + 1);
+		qt_strncpy(new_p, buf + i, k - i + 1);
 
 	s += k - i;
 	i = k;
